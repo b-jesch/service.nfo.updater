c2d955d55d7c6de53210342fce19822d4c7c5fc5
b-jesch <birger.jesch@gmail.com>
2025-01-17 12:30:31 +0100

    Supports multiepisode NFO now

--- default.py
+++ default.py
@@ -6,6 +6,7 @@ import json
 import xml.etree.ElementTree as ElTr
 from xml.dom import minidom
 import os
+import re
 from datetime import datetime
 
 addon = xbmcaddon.Addon()
@@ -34,11 +35,15 @@ def cleanup_xml(xml_string):
     new_lines = []
 
     # remove unwanted empty lines from nfo (different behaviour in Win and Linux)
-    for line in nfo_lines: new_lines.append(line) if line.strip() else None
+    # remove temporary root tag and one indent
+    for line in nfo_lines:
+        line = re.sub('</?nfo[^>]*?>', '', str(line))
+        if line.strip(): new_lines.append(line.replace('\t<', '<'))
     try:
-        return b'\n'.join(new_lines)
-    except TypeError:
         return '\n'.join(new_lines)
+    except TypeError:
+        return b'\n'.join(new_lines)
+
 
 class NFOUpdater(xbmc.Monitor):
     def __init__(self, *args, **kwargs):
@@ -53,8 +58,11 @@ class NFOUpdater(xbmc.Monitor):
         log("Discard notification %s" % method)
 
     def quit(self, method, data):
-        log("System.OnQuit received: %s, exiting application" %data)
-        exit(0)
+        log("System.OnQuit received: %s, exiting application" % data)
+        try:
+            exit(0)
+        except SystemExit:
+            pass
 
     def onNotification(self, sender, method, data):
         self.methodDict.get(method, self.err)(method, data)
@@ -62,36 +70,36 @@ class NFOUpdater(xbmc.Monitor):
     def videolibrary_onupdate(self, method, data):
         log("Notification received: %s - %s" % (method, data))
         j_data = json.loads(data)
-        try:
-            item = j_data['item']
 
-            if item['type'] == 'movie':
-                mediaquery = "VideoLibrary.GetMovieDetails"
-                mediatype = "movieid"
+        try:
+            item = j_data.get('item', None)
+            if item is None:
+                raise KeyError('Required data not included in JSON query')
+            elif item['type'] == 'movie':
                 details = "moviedetails"
+                nfo_root = 'movie'
+                query = {"method": "VideoLibrary.GetMovieDetails", "params": {"movieid": item['id'], "properties": ["file"]}}
             elif item['type'] == 'musicvideo':
-                mediaquery = "VideoLibrary.GetMusicVideoDetails"
-                mediatype = "musicvideoid"
                 details = "musicvideodetails"
+                nfo_root = 'musicvideo'
+                query = {"method": "VideoLibrary.GetMusicVideoDetails", "params": {"musicvideoid": item['id'], "properties": ["file"]}}
             elif item['type'] == 'episode':
-                mediaquery = "VideoLibrary.GetEpisodeDetails"
-                mediatype = "episodeid"
                 details = "episodedetails"
+                nfo_root = 'episodedetails'
+                query = {"method": "VideoLibrary.GetEpisodeDetails", "params": {"episodeid": item['id'], "properties": ["file", "episode"]}}
             else:
                 raise KeyError('Video library type \'%s\' not supported' % item['type'])
 
-            query = {"method": mediaquery, "params": {mediatype: item['id'], "properties": ["file"]}}
             result = jsonrpc(query)
-
             if result is not None:
-                self.update_nfo(result[details], j_data['playcount'], item['type'])
+                self.update_nfo(result[details], j_data.get('playcount', 0), item['type'], nfo_root)
 
         except KeyError as e:
             log('Key error: %s' % e, level=xbmc.LOGWARNING)
             return False
 
     @staticmethod
-    def update_nfo(data, playcount, data_type):
+    def update_nfo(data, playcount, data_type, nforoot):
 
         nfo = "%s.nfo" % os.path.splitext(data['file'])[0]
 
@@ -104,33 +112,43 @@ class NFOUpdater(xbmc.Monitor):
             raise FileNotFoundError
 
         try:
-            with xbmcvfs.File(nfo, 'r') as nfo_file: nfo_xml = nfo_file.read()
-            xml = ElTr.ElementTree(ElTr.fromstring(cleanup_xml(nfo_xml)))
+            with xbmcvfs.File(nfo, 'r') as nfo_file:
+                nfo_xml = nfo_file.read()
+
+            # make Kodi's NFOs valid by adding a temporal root element as multiepisode NFOs are malformed XMLs
+            xml = ElTr.ElementTree(ElTr.fromstring(re.sub(r"(<\?xml[^>]+\?>)", r"\1<nfo>", nfo_xml) + "</nfo>"))
             root = xml.getroot()
+            for tag in root.findall(nforoot):
 
-            # looking for tag 'watched', create it if necessary and set content depending on playcount
-            xml_watched = ElTr.SubElement(root, 'watched') if root.find('watched') is None else root.find('watched')
-            xml_watched.text = "true" if playcount > 0 else "false"
+                # find episode number in episode of single/multiepisode or modify other nfo
+                if tag.find('episode') is None or (
+                        tag.find('episode') is not None and tag.find('episode').text == str(data.get('episode', None))):
 
-            # add 'lastplayed' element if playcount > 0
-            if playcount > 0:
-                xml_lastplayed = ElTr.SubElement(root, 'lastplayed') if root.find('lastplayed') is None else root.find('lastplayed')
-                xml_lastplayed.text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-            else:
-                if root.find('lastplayed') is not None: root.remove(root.find('lastplayed'))
+                    # looking for tag 'watched', create it if necessary and set content depending on playcount
+                    xml_watched = ElTr.SubElement(tag, 'watched') if tag.find('watched') is None else tag.find(
+                        'watched')
+                    xml_watched.text = "true" if playcount > 0 else "false"
 
-            # looking for tag 'playcount' create it if necessary and set content with playcount
-            xml_playcount = ElTr.SubElement(root, 'playcount') if root.find('playcount') is None else root.find('playcount')
-            xml_playcount.text = str(playcount)
+                    # add 'lastplayed' element if playcount > 0
+                    if playcount > 0:
+                        xml_lastplayed = ElTr.SubElement(tag, 'lastplayed') if tag.find('lastplayed') is None else tag.find('lastplayed')
+                        xml_lastplayed.text = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                    else:
+                        if tag.find('lastplayed') is not None: tag.remove(tag.find('lastplayed'))
 
-            # convert to pretty formatted xml and write out
-            pretty_xml = minidom.parseString(ElTr.tostring(root)).toprettyxml(indent='\t', newl=os.linesep, encoding='UTF-8')
-            with xbmcvfs.File(nfo, 'w') as f: result = f.write(cleanup_xml(pretty_xml))
+                    # looking for tag 'playcount' create it if necessary and set content with playcount
+                    xml_playcount = ElTr.SubElement(tag, 'playcount') if tag.find('playcount') is None else tag.find('playcount')
+                    xml_playcount.text = str(playcount)
+
+            # convert to pretty formatted xml, remove temporal root element and write out
+            pretty_xml = minidom.parseString(ElTr.tostring(root)).toprettyxml(indent='\t', newl=os.linesep)
+            with xbmcvfs.File(nfo, 'w') as f:
+                result = f.write(cleanup_xml(pretty_xml))
             if result:
                 log('NFO %s written.' % nfo)
                 return True
             else:
-                xbmcgui.Dialog().notification(addon_name, 'NFO update/creation failed.')
+                xbmcgui.Dialog().notification(addon_name, 'NFO update/creation failed.', xbmcgui.NOTIFICATION_ERROR)
                 raise PermissionError('NFO update/creation failed.')
 
         except (ElTr.ParseError, FileNotFoundError, PermissionError) as e:
@@ -138,7 +156,7 @@ class NFOUpdater(xbmc.Monitor):
             return False
 
     # main loop
-    
+
     def main(self):
         while not self.abortRequested():
             self.waitForAbort(10000)
